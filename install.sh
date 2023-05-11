a88ea7233b81082abc2020b5e4755cdc7bedfdf5
LUIS EDUARDO SOLANO LOPEZ <lesolano@pichincha.com>
2023-05-11 17:50:16 -0500

    update commands install.sh

--- install.sh
+++ install.sh
@@ -13,17 +13,23 @@ sudo yum install git -y
 #Confirmar version de Git
 git --version
 
-#levantamiento contenedores petplate
+#realizar login de docker (cambiar las variables que estan entre <>)
+sudo docker login
+DOCKER_USERNAME=<tu_nombre_de_usuario_de_Docker_Hub>
+DOCKER_PASSWORD=<tu_contraseña_de_Docker_Hub>
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+#levantamiento contenedores petplate despues de realizar push del front
 sudo docker pull postgres
 sudo docker pull mongo
-sudo docker pull brayanesq/pet-plate-frontend:v1.5 #hacer pull al contenedor propio#
+sudo docker pull lucho0841/pet-plate-frontend:v1.5 #hacer pull al contenedor propio#
 sudo docker pull brayanesq/pet-plate-frontend:v1.5
 sudo docker pull brayanesq/pet-plate-backend2:v1.1
 sudo docker pull brayanesq/pet-plate-chatbot:v1.3
 
 #Despues de crear y configurar el docker-compose, instalar las dependencias del mismo
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/loca>
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo docker-compose --version
 
